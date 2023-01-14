b904c99f5f66180489e13df135b0f22e06986c17
Ivan Rodin <ivs.rodin@gmail.com>
2023-01-14 22:22:03 +0100

    Start migrate to JS with React

---
+++ index.js
@@ -0,0 +1,46 @@
+ReactDOM.render(
+    <div>
+        {/* <div>
+            <div class="container">
+            <nav class="navbar">
+            <ul>
+                <li><a href="#intro">Home</a></li>
+                <li><a href="#group-1">Education</a></li>
+                <li><a href="#group-2">Work</a></li>
+            </ul>
+            </nav>
+        </div>, */}
+
+        <div class="parallax_wrapper">  
+            <div class="parallax_group intro_screen" id="intro">
+            <header class="intro_header">
+                <div class="intro_caption">Welcome to my website</div>
+                <div class="intro_title">Ivan Rodin</div>
+            </header>
+            </div>
+
+
+            <div class="parallax_group" id="group-1">
+            <div class="parallax_layer base_layer">
+                Education
+            </div>
+            <div class="parallax_layer mid_layer">
+                Comming soon...
+            </div>
+            </div>
+
+            <div class="parallax_group" id="group-2">
+            <div class="parallax_layer mid_layer">
+                Work
+            </div>
+            <div class="parallax_layer top_layer">
+                Comming soon...
+            </div>
+            </div>
+
+
+            <div class="parallax_group outro_screen" id="outro">
+            The end
+            </div>
+        </div>
+    </div> , document.getElementById("root"))
