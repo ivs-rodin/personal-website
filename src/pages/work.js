a04f235666edeb4c0a2b48084f8a26d90edd9478
Ivan Rodin <ivs.rodin@gmail.com>
2023-01-24 23:12:08 +0100

    Add some parallax style

---
+++ src/pages/work.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Component } from 'react';
+
+import '../css/App.css'
+
+import { useParallax } from "react-scroll-parallax";
+
+const Work = () => {
+
+    const { ref } = useParallax({ speed: 400 });
+  
+    return (
+        <div ref={ref} className="work_page">
+            <div className="work_group">
+                <header className="work_header">
+                    Comming soon...
+                </header>
+            </div>
+        </div>
+    );
+  }
+
+
+export default Work;
